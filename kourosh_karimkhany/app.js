77e023ee5406c0842f427606369960f327b5bc3f
Kourosh Karimkhany <kouroshk@gmail.com>
2014-10-06 17:46:13 -0700

    initial commit

--- kourosh_karimkhany/app.js
+++ kourosh_karimkhany/app.js
@@ -1,1 +1,16 @@
-//Your code here
+// Apply event listeners to each square
+var squares = document.getElementsByTagName("td");
+counter = 0;
+for (var i = 0; i < squares.length; i++) {
+	squares[i].addEventListener("click", function() {
+		if (counter % 2 === 0 && this.getAttribute("class") === "blank") {
+			this.innerHTML = "X";
+			this.setAttribute("class", "filled");
+			counter++;
+		} else if (counter % 2 !== 0 && this.getAttribute('class') === "blank") {
+			this.innerHTML = "O";
+			this.setAttribute("class", "filled");
+			counter++;
+		}
+	});
+}
